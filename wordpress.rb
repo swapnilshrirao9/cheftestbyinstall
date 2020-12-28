cf751497c4abbdfb176e043314024ae4597a62a8
swapnilshrirao9 <67373271+swapnilshrirao9@users.noreply.github.com>
2020-12-28 14:25:00 +0530

    Create wordpress.rb

---
+++ wordpress.rb
@@ -0,0 +1,42 @@
+execute "update" do
+  command "apt update -y"
+end
+package "apache2" do
+  action :install
+end
+service "apache2" do
+  action :start
+end
+package %w(mysql-server mysql-client php libapache2-mod-php php-mcrypt php-mysql) do
+  action :install
+end
+execute "password" do
+  command "mysqladmin -u root password rootpassword"
+end
+remote_file "mysqlcommand" do
+    source 'https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/mysqlcommands'
+    path "/tmp/mysqlcommands"
+end
+execute "rootpassword" do
+  command "mysql -uroot -prootpassword < /tmp/mysqlcommands"
+end
+remote_file "wordpress" do
+    source 'https://wordpress.or/var/www/html/wordpress
+end
+execute "unzip" do
+ command "unzip /tmp/latest.zip -d /var/www/html"
+end
+remote_file "php" do
+    source 'https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/wp-config-sample.php'
+end
+execute "copy" do
+ command "cp wp-config-sample.php /var/www/html/wordpress/wp-config.php"
+end
+file '/var/www/html/wordpress' do
+  mode '0755'
+  owner 'www-data'
+  group 'www-data'
+end
+service "apache2" do
+  action :restart
+end
